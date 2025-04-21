23cbdd092c68a6dc4222308ba4a73242f6a4b9d4
Fakhar27 <fakharmursaleen12345@gmail.com>
2025-04-21 22:32:06 +0500

    MORE CHANGES FOR ECS DEPLOYEMENT!!

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,11 +35,21 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-# ]
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+# # CORS_ALLOWED_ORIGINS = [
+# #     "http://localhost:3000",
+# # ]
+# CORS_ALLOW_CREDENTIALS = True
+
+# Add to ALLOWED_HOSTS
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'backend']
+
+# Update CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://frontend:3000",
+]
 
 ROOT_URLCONF = 'backend.urls'
 
